a57c253be780feb090601f0963b1024fd91df214
TGyAkos <85934789+TGyAkos@users.noreply.github.com>
2024-12-04 00:18:31 +0100

    init

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['log', 'fatal', 'error', 'warn', 'debug', 'verbose'],
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  app.use(cookieParser());
+
+  await app.listen(process.env.PORT ?? 3300);
+
+  const logger = new Logger('Main');
+  logger.log(`Server started on port ${process.env.PORT ?? 3300}`);
 }
 bootstrap();
