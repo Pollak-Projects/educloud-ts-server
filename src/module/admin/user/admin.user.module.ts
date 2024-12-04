4cb0cf1bb53286e4ead09fa59e1068b264a5bc43
TGyAkos <85934789+TGyAkos@users.noreply.github.com>
2024-12-04 20:13:43 +0100

    added role to admin.user.module.ts

--- src/module/admin/user/admin.user.module.ts
+++ src/module/admin/user/admin.user.module.ts
@@ -6,9 +6,10 @@ import { UserData } from 'src/module/user/user.data.entity';
 import { Teacher } from 'src/module/teacher/teacher.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from '../../auth/auth.module';
+import { Role } from '../../role/role.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([User, UserData, Teacher]), AuthModule],
+    imports: [TypeOrmModule.forFeature([User, UserData, Teacher, Role]), AuthModule],
     controllers: [AdminUserController],
     providers: [AdminUserService],
 })
