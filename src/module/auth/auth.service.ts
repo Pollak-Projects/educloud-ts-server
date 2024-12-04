a7c471ac0116b92448c72a3f456fb9852c40716a
TGyAkos <t.gyula.akos@gmail.com>
2024-12-04 14:20:32 +0100

    started adding auth

--- src/module/auth/auth.service.ts
+++ src/module/auth/auth.service.ts
@@ -1,12 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
 import { JwtService } from '@nestjs/jwt';
 import { User } from '../user/user.entity';
+import * as bcrypt from 'bcryptjs';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class AuthService {
+    private readonly logger = new Logger(AuthService.name);
     constructor(
         private jwtService: JwtService,
 
@@ -15,10 +17,23 @@ export class AuthService {
     ) {}
 
     async login(loginDto: LoginDto): Promise<string> {
+        this.logger.verbose(`Login attempt by ${JSON.stringify(loginDto)}`);
 
+        const user = await this.userRepository.findOneBy({ username: loginDto.username })
+            .catch(e => {
+            this.logger.error(`User ${JSON.stringify(loginDto)} does not exist`);
+            throw new HttpException(`Invalid authnetication data`, HttpStatus.UNAUTHORIZED);
+        });
+
+        if (!user) {
+            this.logger.error(`User ${JSON.stringify(loginDto)} does not exist`);
+            throw new HttpException(`Invalid authentication data`, HttpStatus.UNAUTHORIZED);
+        }
         
-
-        this.userRepository.findOneBy({  });
+        if (!(await bcrypt.compare(loginDto.password, user.hashedPwd))) {
+            this.logger.error(`Password for ${JSON.stringify(loginDto)} is incorrect`);
+            throw new HttpException(`Invalid authentication data`, HttpStatus.UNAUTHORIZED);
+        }
 
 
         return JSON.stringify(LoginDto);
