1b2f3167e95680e9f1d265ee452dba45a3f3d2a5
valid <janipatrik138@gmail.com>
2024-12-04 10:34:48 +0100

    implemented complete assignment routes for admin

--- src/module/admin/assignment/assignment.controller.ts
+++ src/module/admin/assignment/assignment.controller.ts
@@ -1,12 +1,43 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Query, Body } from '@nestjs/common';
 import { AssignmentService } from './assignment.service';
+import { AssignmentDto } from './dto/assignment.dto';
 
 @Controller('api/admin/assignment')
 export class AssignmentController {
   constructor(private readonly appService: AssignmentService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('get-all')
+  getAllAssignments(): string {
+    return this.appService.getAllAssignments();
+  }
+
+  @Get('get-by-id')
+  getAssignmentById(@Query('id') id: number): string {
+    return this.appService.getAssignmentById(id);
+  }
+
+  @Get('get-by-name')
+  getAssignmentByName(@Query('name') name: string): string {
+    return this.appService.getAssignmentByName(name);
+  }
+
+  @Get('get-by-filter')
+  getAssignmentByFilter(@Query('category') category: string, @Query('grade') grade: string, @Query('profession') profession: string): string {
+    return this.appService.getAssignmentByFilter(category, grade, profession);
+  }
+
+  @Post('create')
+  createAssignment(@Body() AssignmentBody: AssignmentDto): string {
+    return this.appService.createAssignment(AssignmentBody);
+  }
+
+  @Put('update-by-id')
+  updateAssignmentById(@Query('id') id: number, @Body() AssignmentBody: AssignmentDto): string {
+    return this.appService.updateAssignmentById(id, AssignmentBody);
+  }
+
+  @Delete('delete-by-id')
+  deleteAssignmentById(@Query('id') id: number): string {
+    return this.appService.deleteAssignmentById(id);
   }
 }
--- src/module/admin/assignment/assignment.service.ts
+++ src/module/admin/assignment/assignment.service.ts
@@ -1,8 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { AssignmentDto } from './dto/assignment.dto';
 
 @Injectable()
 export class AssignmentService {
-  getHello(): string {
+  getAllAssignments(): string {
+    return 'Hello World!';
+  }
+
+  getAssignmentById(id: number): string {
+    return 'Hello World!';
+  }
+
+  getAssignmentByName(name: string): string {
+    return 'Hello World!';
+  }
+
+  getAssignmentByFilter(category: string, grade: string, profession: string): string {
+    return 'Hello World!';
+  }
+
+  createAssignment(AssignmentBody: AssignmentDto): string {
+    return 'Hello World!';
+  }
+
+  updateAssignmentById(id: number, AssignmentBody: AssignmentDto): string {
+    return 'Hello World!';
+  }
+
+  deleteAssignmentById(id: number): string {
     return 'Hello World!';
   }
 }
