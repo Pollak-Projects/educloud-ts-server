320bf78c0133b6ac04cfe649ea78c18a84c3e87b
valid <janipatrik138@gmail.com>
2024-12-04 12:18:39 +0100

    commented docker + updated package.sjon

--- src/module/admin/admin.module.ts
+++ src/module/admin/admin.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
-import { AdminAssignmentModule } from '../assignment/admin.assignment.module';
-import { AdminUserModule } from '../user/admin.user.module';
+import { AdminAssignmentModule } from './assignment/admin.assignment.module';
+import { AdminUserModule } from './user/admin.user.module';
 import { AdminModuleModule } from './module/admin.module.module';
 
 @Module({
