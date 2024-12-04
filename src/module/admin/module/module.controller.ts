956948ff09d51ab36c699b7a1247210565c5ac4f
valid <janipatrik138@gmail.com>
2024-12-04 10:30:44 +0100

    implemented complete module routes

--- src/module/admin/module/module.controller.ts
+++ src/module/admin/module/module.controller.ts
@@ -1,12 +1,43 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Put, Body, Query } from '@nestjs/common';
 import { ModuleService } from './module.service';
+import { ModuleDto } from './dto/module.dto';
 
 @Controller('api/admin/module')
 export class ModuleController {
   constructor(private readonly appService: ModuleService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('get-all')
+  getAllModules(): string {
+    return this.appService.getAllModules();
+  }
+
+  @Get('get-by-id')
+  getModuleById(@Query('id') id: number): string {
+    return this.appService.getModuleById(id);
+  }
+
+  @Get('get-by-name')
+  getModuleByName(@Query('name') name: string): string {
+    return this.appService.getModuleByName(name);
+  }
+
+  @Get('get-by-filter')
+  getModuleByFilter(@Query('category') category: string, @Query('grade') grade: string, @Query('profession') profession: string): string {
+    return this.appService.getModuleByFilter(category, grade, profession);
+  }
+
+  @Post('create')
+  createModule(@Body() moduleBody: ModuleDto): string {
+    return this.appService.createModule(moduleBody);
+  }
+
+  @Put('update-by-id')
+  updateModuleById(@Query('id') id: number, @Body() moduleBody: ModuleDto): string {
+    return this.appService.updateModuleById(id, moduleBody);
+  }
+
+  @Delete('delete-by-id')
+  deleteModuleById(@Query('id') id: number): string {
+    return this.appService.deleteModuleById(id);
   }
 }
--- src/module/admin/module/module.service.ts
+++ src/module/admin/module/module.service.ts
@@ -1,8 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { ModuleDto } from './dto/module.dto';
 
 @Injectable()
 export class ModuleService {
-  getHello(): string {
+  getAllModules(): string {
+    return 'Hello World!';
+  }
+
+  getModuleById(id: number): string {
+    return 'Hello World!';
+  }
+
+  getModuleByName(name: string): string {
+    return 'Hello World!';
+  }
+
+  getModuleByFilter(category: string, grade: string, profession: string): string {
+    return 'Hello World!';
+  }
+
+  createModule(moduleBody: ModuleDto): string {
+    return 'Hello World!';
+  }
+
+  updateModuleById(id: number, moduleBody: ModuleDto): string {
+    return 'Hello World!';
+  }
+
+  deleteModuleById(id: number): string {
     return 'Hello World!';
   }
 }
