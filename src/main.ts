73228efc815b056ad089d8db0ef447d70e3b2ef7
TGyAkos <85934789+TGyAkos@users.noreply.github.com>
2024-12-18 08:27:22 +0100

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
-import * as session from 'express-session';
+import * as process from 'node:process';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, {
@@ -21,9 +22,23 @@ async function bootstrap() {
 
     app.setGlobalPrefix('api');
 
-    await app.listen(process.env.PORT ?? 3300);
-
     const logger = new Logger('Main');
+
+    if (process.env.DEV === 'true') {
+        logger.verbose('Starting in DEV mode');
+        const config = new DocumentBuilder()
+            .setTitle('Educloud API')
+            .setDescription('The Educloud API description')
+            .setVersion('1.0')
+            .addTag('Educloud')
+            .build();
+        const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+        SwaggerModule.setup('swagger', app, documentFactory);
+        logger.debug(`Swagger started on port ${process.env.PORT ?? 3300}/swagger`);
+    }
+
+    await app.listen(process.env.PORT ?? 3300);
     logger.log(`Server started on port ${process.env.PORT ?? 3300}`);
 }
 
